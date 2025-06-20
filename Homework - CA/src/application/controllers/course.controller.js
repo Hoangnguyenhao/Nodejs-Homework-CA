86de766db857e663a29e8936b7ca0ff4a0752f00
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-06-20 14:37:29 +0700

    Update course.controller.js

---
+++ Homework - CA/src/application/controllers/course.controller.js
@@ -0,0 +1,9 @@
+class CourseController {
+  constructor({ uc }) { this.uc = uc; }
+  async create(req, res) { res.status(201).json(await this.uc.create(req.body)); }
+  async getAll(req, res) { res.json(await this.uc.getAll()); }
+  async getById(req, res) { res.json(await this.uc.getById(req.params.id)); }
+  async update(req, res) { res.json(await this.uc.update(req.params.id, req.body)); }
+  async delete(req, res) { await this.uc.delete(req.params.id); res.status(204).end(); }
+}
+module.exports = CourseController;
